b76e9e4dc9ac6181151a77b01c40d948facf55db
sumit.p <sumit.p8432@gmail.com>
2025-09-30 02:20:38 +0530

    locator fixed

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -8,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	WebDriver driver;
